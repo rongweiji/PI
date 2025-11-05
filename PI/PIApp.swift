031681f8332ad3c415a88227b9ad315f90b52774
wayne <lycazure@hotmail.com>
2025-11-05 12:06:50 -0600

    Initial Commit

---
+++ PI/PIApp.swift
@@ -0,0 +1,32 @@
+//
+//  PIApp.swift
+//  PI
+//
+//  Created by Rongwei Ji on 11/5/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PIApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ PITests/PITests.swift
@@ -0,0 +1,17 @@
+//
+//  PITests.swift
+//  PITests
+//
+//  Created by Rongwei Ji on 11/5/25.
+//
+
+import Testing
+@testable import PI
+
+struct PITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
